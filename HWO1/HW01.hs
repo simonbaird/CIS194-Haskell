e3a3904e9660e0bee448e61f5cd870913a8eb659
Simon Baird <simon.baird@gmail.com>
2014-11-04 23:10:25 +1000

    Finish up validate check digit in Homework 1

--- HWO1/HW01.hs
+++ HWO1/HW01.hs
@@ -1,23 +1,17 @@
 {-
-Name: Simon Baird
-Collaborators:
-Notes:
-        what's not working, what's really cool, etc.>
+  Name: Simon Baird
+  Collaborators:
+  Notes:
 -}
-
-module HW01 where         -- We'll learn more about this later
+module HW01 where -- We'll learn more about this later
 
 isThisWorking :: String
 isThisWorking = "Yes"
 -- Load this file into GHCi (say, with `ghci HW01.hs`) and type
 -- `isThisWorking` at the prompt. GHCi will tell you whether it's working!
 
--- Put your work below.
-lastDigit :: Integer -> Integer
-dropLastDigit :: Integer -> Integer
-
 --
--- Exercise 1
+-- Exercise 1 (first version)
 --
 intToStr :: Integer -> String
 intToStr n = show n
@@ -28,12 +22,23 @@ strToInt n = read n :: Integer
 charToInt :: Char -> Integer
 charToInt c = strToInt([c])
 
-lastDigit n = charToInt(last(intToStr(n)))
-dropLastDigit n = strToInt(reverse(tail(reverse(intToStr(n)))))
+lastDigit1 :: Integer -> Integer
+lastDigit1 n = charToInt(last(intToStr(n)))
 
+dropLastDigit1 :: Integer -> Integer
+dropLastDigit1 n = strToInt(reverse(tail(reverse(intToStr(n)))))
 
 --
--- Exercise 2
+-- Exercise 1 (with math functions as suggested)
+--
+lastDigit :: Integer -> Integer
+lastDigit n = n `mod` 10
+
+dropLastDigit :: Integer -> Integer
+dropLastDigit n = n `div` 10
+
+--
+-- Exercise 2 (first version)
 --
 strToDigits :: String -> [Integer]
 strToDigits [] = []
@@ -44,9 +49,41 @@ toDigits1 n
  | n < 1     = []
  | otherwise = strToDigits(intToStr(n))
 
--- Probably supposed to use dropLastDigit...
+--
+-- Exercise 2 (using dropLastDigit and lastDigit)
+--
 toDigits :: Integer -> [Integer]
 toDigits n
  | n < 1     = []
  | n < 10    = [n]
  | otherwise = toDigits(dropLastDigit(n)) ++ [lastDigit(n)]
+
+--
+-- Exercise 3
+--
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther [] = []
+doubleEveryOther (n:ns) =
+  (if length(ns) `mod` 2 == 0 then n else n*2) : doubleEveryOther(ns)
+
+--
+-- Exercise 4
+--
+sumDigits :: [Integer] -> Integer
+sumDigits [] = 0
+sumDigits (n:ns) =
+  (if n < 10 then n else sumDigits(toDigits(n))) + sumDigits(ns)
+
+--
+-- Exercise 5
+--
+validate :: Integer -> Bool
+validate n =
+  lastDigit(sumDigits(doubleEveryOther(toDigits(n)))) == 0
+
+--
+-- For checking some validations
+--
+e1 = 4012888888881881 -- true
+e2 = 4012888888881882 -- false
+e3 = 4111111111111111 -- true
